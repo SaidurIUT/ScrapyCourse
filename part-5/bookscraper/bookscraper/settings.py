54c0f5a3a5eda7f5e886f53b03372a95f948bbba
MD Saidur Rahman Sagor <mdsaidurrahmansagor@MDs-Mac-mini.local>
2026-01-04 15:04:19 +0600

    adds db

--- part-5/bookscraper/bookscraper/settings.py
+++ part-5/bookscraper/bookscraper/settings.py
@@ -58,9 +58,19 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "bookscraper.pipelines.BookscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "bookscraper.pipelines.BookscraperPipeline": 300,
+   'bookscraper.pipelines.SaveToMySQLPipeline': 400,
+
+}
+
+FEEDS = {
+    'booksdata.csv': {
+      'format': 'csv', 
+      'overwrite': True,
+      'encoding': 'utf-8', 
+   }
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
